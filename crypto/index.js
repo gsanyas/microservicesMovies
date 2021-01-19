0e35a4353691e680feb88bd83f8b394f399acdee
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-19 15:34:43 +0100

    feat: encrypt and decrypt token

--- crypto/index.js
+++ crypto/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const config = require('./config.json');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const config = require("./config.json");
 
 const app = express();
 app.use(cors({ credentials: true, origin: config.origin }));
@@ -9,8 +10,64 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.listen(config.port, () => {
-    console.log('Server started !');
+    console.log("Server started !");
 });
 
-app.get('/encryptUser', (_req, res) => res.sendStatus(200))
-app.get('/decryptUser', (_req, res) => res.sendStatus(200))
+const readFile = () => {
+    try {
+        const secret = fs.readFileSync("./sym_keyfile.key", "utf8");
+        return secret;
+    } catch (_err) {
+        console.error(
+            "Unable to open the keyfile. Be sure you provided 'sym_keyfile.key' in the root folder of the crypto module."
+        );
+    }
+};
+
+const createToken = (user) => {
+    try {
+        const secret = readFile();
+        const token = jwt.sign(user, secret, { algorithm: "HS256" });
+        return token;
+    } catch (_err) {
+        console.error("Unable to sign the token.");
+    }
+};
+
+const readToken = (token) => {
+    try {
+        const secret = readFile();
+        jwt.verify(token, secret, (err, data) => {
+            if (err) {
+                throw new Error("Invalid token.");
+            } else if (data) {
+                return data;
+            }
+        });
+    } catch (_err) {
+        console.error("Unable to verify the token.");
+    }
+};
+
+app.get("/encryptUser", (req, res) => {
+    const user = req.body.user;
+    if (user) {
+        const token = createToken(user);
+        res.status(200).send(token);
+    } else {
+        res.sendStatus(404);
+    }
+});
+app.get("/decryptUser", (req, res) => {
+    const token = req.body.token;
+    if (token) {
+        try {
+            const user = readToken(token);
+            res.status(200).send(user);
+        } catch (_err) {
+            res.sendStatus(401);
+        }
+    } else {
+        res.sendStatus(401);
+    }
+});
