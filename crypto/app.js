a11695f95ce83a05573673efdf4a1a0b7b8139c3
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-19 16:10:33 +0100

    style: separate token services

--- crypto/app.js
+++ crypto/app.js
@@ -3,47 +3,12 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const config = require("./config.json");
+const { readToken, createToken } = require("./tokenService");
 
 const app = express();
 app.use(cors({ credentials: true, origin: config.origin }));
 app.use(cookieParser());
 app.use(express.json());
-
-const readFile = () => {
-    try {
-        const secret = fs.readFileSync("./sym_keyfile.key", "utf8");
-        return secret;
-    } catch (_err) {
-        console.error(
-            "Unable to open the keyfile. Be sure you provided 'sym_keyfile.key' in the root folder of the crypto module."
-        );
-    }
-};
-
-const createToken = (user) => {
-    try {
-        const secret = readFile();
-        const token = jwt.sign(user, secret, { algorithm: "HS256" });
-        return token;
-    } catch (_err) {
-        console.error("Unable to sign the token.");
-    }
-};
-
-const readToken = (token) => {
-    try {
-        const secret = readFile();
-        jwt.verify(token, secret, (err, data) => {
-            if (err) {
-                throw new Error("Invalid token.");
-            } else if (data) {
-                return data;
-            }
-        });
-    } catch (_err) {
-        console.error("Unable to verify the token.");
-    }
-};
 
 app.get("/encryptUser", (req, res) => {
     const user = req.params.user;
---
+++ crypto/tokenService.js
@@ -0,0 +1,35 @@
+const readFile = () => {
+    try {
+        const secret = fs.readFileSync("./sym_keyfile.key", "utf8");
+        return secret;
+    } catch (_err) {
+        console.error(
+            "Unable to open the keyfile. Be sure you provided 'sym_keyfile.key' in the root folder of the crypto module."
+        );
+    }
+};
+
+exports.createToken = (user) => {
+    try {
+        const secret = readFile();
+        const token = jwt.sign(user, secret, { algorithm: "HS256" });
+        return token;
+    } catch (_err) {
+        console.error("Unable to sign the token.");
+    }
+};
+
+exports.readToken = (token) => {
+    try {
+        const secret = readFile();
+        jwt.verify(token, secret, (err, data) => {
+            if (err) {
+                throw new Error("Invalid token.");
+            } else if (data) {
+                return data;
+            }
+        });
+    } catch (_err) {
+        console.error("Unable to verify the token.");
+    }
+};
