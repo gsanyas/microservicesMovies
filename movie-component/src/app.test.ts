27a034c3bd92123413b9787d7ed47210a33bc25b
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-29 00:08:27 +0100

    test: findTitle

--- movie-component/src/app.test.ts
+++ movie-component/src/app.test.ts
@@ -1,7 +1,7 @@
 import request from "supertest"
 import { app } from "./app"
 import movies from "./data/movies.json"
-import { Movie } from "./model"
+import { createMovie, isMovie, Movie } from "./model"
 import { copyObject } from "./utils"
 
 let savedMovies: Movie[]
@@ -36,6 +36,8 @@ const restore = () => {
     restoreArchive()
 }
 
+const validConfig = { Accept: "application/json", rights: "3" }
+
 describe("POST /add", () => {
     beforeEach(save)
     afterEach(restore)
@@ -49,22 +51,56 @@ describe("POST /add", () => {
     test("It should return a 404 status when no movie field in body", () => {
         request(app)
             .post("/add")
-            .set({ Accept: "application/json", rights: "3" })
+            .set(validConfig)
             .send({ uselessField: "uselessValue" })
             .expect(404)
     })
     test("It should return a 415 error status when not good movie type", () => {
         request(app)
             .post("/add")
-            .set({ Accept: "application/json", rights: "3" })
+            .set(validConfig)
             .send({ movie: { title: "testAdd", director: "testAddDirector" } })
             .expect(415)
     })
     test("It should return a 201 created status with a movie when everything is correct", () => {
         request(app)
             .post("/add")
-            .set({ Accept: "application/json", rights: "3" })
-            .send({ movie: { title: "testAdd", director: "testAddDirector", genre: "testAddGenre" } })
+            .set(validConfig)
+            .send({
+                movie: {
+                    title: "testAdd",
+                    director: "testAddDirector",
+                    genre: "testAddGenre",
+                },
+            })
             .expect(201)
+    })
+})
+
+describe("GET /find/:title", () => {
+    beforeAll(() => {
+        save()
+        createMovie({
+            title: "testFindTitle",
+            director: "testFindDirector",
+            genre: "testFindGenre",
+        })
+    })
+    afterAll(restore)
+    test("It should return a 401 error without the right authorization", () => {
+        request(app)
+            .get("/find/testFindTitle")
+            .set({ Accept: "application/json" })
+            .expect(401)
+    })
+    test("It should return a movie", async () => {
+        const result = await request(app)
+            .get("/find/testFindTitle")
+            .set(validConfig)
+            .expect(200)
+        expect(isMovie(result.body)).toBe(true)
+    })
+    test("It should return a 404 error when it cannot find the movie", () => {
+        request(app).get("/find/testWrongTitle").set(validConfig).expect(404)
     })
 })
