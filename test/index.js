b52bc8ba1cf0e57d44c80337ff5d7b8d671831be
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-29 18:34:50 +0100

    test: finished main project test page

--- test/index.js
+++ test/index.js
@@ -17,14 +17,16 @@ const catalogCreationResponse = await axios.post(
     interface + "user/add",
     {
         user: {
-            address: "catalog@mail.com",
+            address: "catalog@email.com",
             password: "cata",
             rights: "3",
         },
     },
     { headers: { cookie: adminCookie } }
 );
-console.log("Created catalog admin: " + JSON.stringify(catalogCreationResponse.data));
+console.log(
+    "Created catalog admin: " + JSON.stringify(catalogCreationResponse.data)
+);
 
 // Login with catalog admin account
 
@@ -36,8 +38,46 @@ console.log("Catalog cookie: " + JSON.stringify(catalogCookie));
 
 // Add a movie with catalog account
 
-// Create a client account
+const movieCreationResponse = await axios.post(
+    interface + "movie/add",
+    {
+        movie: {
+            title: "Tenet",
+            director: "Nolan",
+            genre: "sci-fi",
+        },
+    },
+    { headers: { cookie: catalogCookie } }
+);
+console.log("Created movie: " + JSON.stringify(movieCreationResponse.data));
+
+// Create a client account with service admin
+
+const clientCreationResponse = await axios.post(
+    interface + "user/add",
+    {
+        user: {
+            address: "client@email.com",
+            password: "client",
+            rights: "1",
+        },
+    },
+    { headers: { cookie: adminCookie } }
+);
+console.log("Created client: " + JSON.stringify(clientCreationResponse.data));
 
 // Login with client account
 
+const responseClient = await axios.get(
+    interface + "user/login/client@email.com/client"
+);
+const clientCookie = responseClient.headers["set-cookie"];
+console.log("Client cookie: " + JSON.stringify(clientCookie));
+
 // Search for the movie title with client account
+
+const searchResponse = await axios.get(
+    interface + "movie/find/" + movieCreationResponse.data.id,
+    { headers: { cookie: clientCookie } }
+);
+console.log("Obtained movie: " + searchResponse.data);
