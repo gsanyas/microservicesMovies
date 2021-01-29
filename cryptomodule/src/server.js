d5da180ce26ad72efc22c8a1a0467683ec252668
gsanyas <guillem.sanyas@imt-atlantique.net>
2021-01-29 11:59:21 +0100

    chore: use envrionment var in crypto component

--- cryptomodule/src/server.js
+++ cryptomodule/src/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const config = require("../config.json");
 
-app.listen(config.port, () => {
+app.listen(process.env.EXPRESS_PORT, () => {
     console.log("Server started !");
 });
